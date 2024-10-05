1167cc3da890738099bd678d0232c8aa93657cd7
unknown <akasharma03@gmail.com>
2024-10-06 01:41:30 +0530

    Fixed bugs, implemented login functionality, created a cart for each user, and completed the front end.

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+       <p> A good clothing store description will: Use clear and concise language to convey information. Avoid jargon and technical terms that might confuse the reader. Choose words and descriptive adjectives that highlight the unique features of the clothing item.</p>
+       <p>Your fashion site’s product descriptions play a crucial role in attracting and engaging customers – new and old. Your customers want a positive shopping experience, they want to find trust in your brand, and they want to be confident that they’re giving you their money for a good product. Your product page copy can ensure all of that happens. Here’s how to do it</p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default DescriptionBox
